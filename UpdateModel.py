8f2055717e598859efaba80a7b870fb031bbc5c1
warlock921 <caoyu921@163.com>
2017-09-09 20:50:20 +0800

    第二次

--- UpdateModel.py
+++ UpdateModel.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 #coding = utf-8
 
+import os
+import sys
+
+ServerDir = "~/home/wwwroot/"
+ServerBakDir = "~/home/WebBackup/"
+
+def get_foward_dir():
+	return os.getcwd()
+
+def read_dir():
+	pass
+
+def read_bak_dir():
+	pass
+
+def main():
+	pass
