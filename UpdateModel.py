5257477331cc111b45a931b1e7bba569f1c45087
warlock921 <caoyu921@163.com>
2017-09-10 22:48:08 +0800

    修改了主程序文件，增加注释，修改了BUG

--- UpdateModel.py
+++ UpdateModel.py
@@ -8,19 +8,31 @@ from subprocess import *
 ServerDir = "/home/wwwroot/"
 ServerUpdateFileDir = 'UpdateFile'
 ServerBakDir = "BackupFile"
+dirlist = []
 
 def get_foward_dir():
 	return os.getcwd()
 
+#用于读取更新文档目录的方法
 def read_update_dir():
-	dirlist = []
+	#定义需要更新的目录及文件列表
+	#使用os.walk()函数实现遍历当前路径下的所有目录结构，输出为列表，列表值为tuple（元组）
 	for i in os.walk("."):
+		#print(dirlist)
+		#将非空的目录及非顶层的目录保存到列表中
 		if i[-1] != [] and i[0] != ".":
 			dirlist.append(i)
 			#print(dirlist)
+	if dirlist == []:
+		print("\033[1;31m 注意： \033[0mUpdateFile文件夹下不存在可更新文件！按回车键退出！")
+		#print(dirlist)
+		input("")
+		exit()
+
 	dirlist_length = len(dirlist)
 	#print(dirlist_length)
 	list_num = 0
+	#使用循环取出需要更新的目录路径及文件名称列表
 	while list_num < dirlist_length:
 		update_dir = dirlist[list_num][0]
 		print('-'*50)
@@ -29,22 +41,23 @@ def read_update_dir():
 		print("需要更新的文件列表%d：%s"%((list_num+1),update_file))
 		list_num+=1
 
+#用于显示已更新过的文档目录
 def read_updated_dir():
-	dirlist = []
-	for i in os.walk("."):
-		if i[-1] != [] and i[0] != ".":
-			dirlist.append(i)
-			#print(dirlist)
-	dirlist_length = len(dirlist)
+	dirlist2 = []
+	for j in os.walk("."):
+		if j[-1] != [] and j[0] != ".":
+			dirlist2.append(j)
+			#print(dirlist2)
+	dirlist2_length = len(dirlist2)
 	#print(dirlist_length)
-	list_num = 0
-	while list_num < dirlist_length:
-		update_dir = dirlist[list_num][0]
+	list_num2 = 0
+	while list_num2 < dirlist2_length:
+		update_dir2 = dirlist2[list_num2][0]
 		print('-'*50)
-		print("已更新的目录%d：\033[1;33m %s \033[0m"%((list_num+1),update_dir))
-		update_file = dirlist[list_num][-1]
-		print("已更新的文件列表%d：\033[1;31m %s \033[0m"%((list_num+1),update_file))
-		list_num+=1
+		print("已更新的目录%d：\033[1;33m %s \033[0m"%((list_num2+1),update_dir2))
+		update_file2 = dirlist2[list_num2][-1]
+		print("已更新的文件列表%d：\033[1;31m %s \033[0m"%((list_num2+1),update_file2))
+		list_num2+=1
 
 def read_bak_dir():
 	pass
@@ -59,25 +72,53 @@ def restart_server():
 	pass
 
 def main():
-	
+	#update_flag用于表示文件是否已经更新过
 	update_flag = True
+	list_x = []
+
+	#定义当前目录路径
 	forward_dir = get_foward_dir()
-	os.chdir(("."+os.sep+ServerUpdateFileDir))
+
+	#循环遍历当前文件夹,并将当前目录下的文件夹（不包括文件）压入列表
+	for x in os.listdir():
+		if os.path.isdir(x):
+			list_x.append(x)
+
+	#判断是否存在存在UpdateFile文件
+	if ServerUpdateFileDir in list_x:
+		#print("命中\033[1;31m %s \033[0m文件夹！"%ServerUpdateFileDir)
+		#操作更换工作目录，进入需要更新的文件夹
+		os.chdir(("."+os.sep+ServerUpdateFileDir))
+	else:
+		#如果不存在UpdateFile文件，则让用户选择是否建立该文件夹
+		print("\033[1;31m UpdateFile文件夹不存在，请先建立UpdateFile文件夹，否则无法更新！ \033[0m")
+		mkdir_input=input("是否建立UpdateFile文件夹？(y/n):")
+		if mkdir_input == 'y' or mkdir_input == 'Y':
+			os.system('mkdir UpdateFile')
+			print("已经建立UpdateFile文件夹！")
+			os.chdir(("."+os.sep+ServerUpdateFileDir))
+			input('')
+		elif mkdir_input == 'n' or mkdir_input == 'N':
+			print("不建立UpdateFile文件夹，无法进行更新，系统退出！")
+			input('')
+			exit()
+		else:
+			pass
+	
+	#开始循环，更新程序主程序
 	while True:
+
 		os.system('clear')
 		print("****************   红企家园网站-更新程序  v1.01   *****************")
-		print("当前目录：\033[1;33;40m %s \033[0m"%forward_dir)
-		#print("目录输出的类型：",type(forward_dir))
-		#print("执行os.listdir(),结果。。。\n",os.listdir())
 
-		for x in os.listdir():
-			if os.path.isdir(x):
-				if x == ServerUpdateFileDir:
-					print("命中\033[1;31m %s \033[0m文件夹！"%ServerUpdateFileDir)
-
+		#字体打印颜色变更语法：\033[1;33;40m %s \033[0m
+		print("命中\033[1;31m %s \033[0m文件夹！"%ServerUpdateFileDir)
+		print(("当前工作目录：\033[1;33;40m %s \033[0m")%forward_dir)
 		print("当前工作的文件夹：%s"%get_foward_dir())
-		
+
+		#判断是否已经更新过
 		if update_flag:
+			#未更新则执行下面语句
 			read_update_dir()
 			print('*'*50)
 			update_usr_input = input("\033[1;31m 注意 \033[0m是否开始更新文件：\n 1.开始更新 \n 2.不更新 \n 3.退出更新程序 \n 请选择(数字键1/2/3，默认3)：")
@@ -101,8 +142,9 @@ def main():
 				print("系统退出！")
 				exit()
 		else:
+			#已更新过则执行下面的代码
 			print('*'*50)
-			update_usr_input = input("\033[1;31m 注意 \033[0m网站已经更新完成：\n 1.查看更新过的目录 \n 2.退出更新程序 \n 请选择(数字键1/2，默认2)：")
+			update_usr_input = input("\033[1;31m 注意： \033[0m网站已经更新完成：\n 1.查看更新过的目录 \n 2.退出更新程序 \n 请选择(数字键1/2，默认2)：")
 			if update_usr_input == "1":
 				read_updated_dir()
 				input("")
