c8b42df2a1c79404aabf645a7361d38d5c7836e1
warlock921 <caoyu921@163.com>
2017-09-10 20:43:00 +0800

    完成读取目录与文件名

---
+++ UpdateFile/src/vendor/zhiyicx/thinksns-old-code/src/addons/model/UserVerifiedModel.class.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * 用户认证模型 - 数据对象模型.
+ *
+ * @author zivss <guolee226@gmail.com>
+ *
+ * @version TS3.0
+ */
+class UserVerifiedModel extends Model
+{
+    protected $tableName = 'user_verified';
+    //2017.9.8添加统一信用号数据表映射
+    protected $fields = array('id', 'uid', 'usergroup_id', 'user_verified_category_id', 'company', 'realname', 'idcard','company_tyidcard', 'phone', 'info', 'verified', 'attach_id');
+
+    /**
+     * 获取指定用户的认证信息.
+     *
+     * @param array $uids 用户ID
+     *
+     * @return array 指定用户的认证信息
+     */
+    public function getUserVerifiedInfo($uids)
+    {
+        if (empty($uids)) {
+            return array();
+        }
+        $map['uid'] = array('IN', $uids);
+        $map['verified'] = 1;
+        $data = $this->where($map)->getHashList('uid', 'info');
+
+        return $data;
+    }
+
+    public function rmVerifiedGroup($gid)
+    {
+        if (empty($gid)) {
+            return false;
+        }
+        $map['usergroup_id'] = $gid;
+        // $uids = $this->where($map)->getAsFieldArray('uid');
+        $result = $this->where($map)->delete();
+
+        // $this->cleanCache($uids);
+
+        return (bool) $result;
+    }
+
+    public function isVerify($uid)
+    {
+        if (empty($uid)) {
+            return false;
+        }
+        $map['uid'] = $uid;
+        $map['verified'] = 1;
+        $data = $this->where($map)->find();
+
+        return (bool) $data;
+    }
+
+    public function cleanCache($uids)
+    {
+        !is_array($uids) && $uids = explode(',', $uids);
+        foreach ($uids as $uid) {
+            S('user_verified_'.$uid, null);
+        }
+    }
+
+    /**
+     * 获取用户认证状态
+     *
+     * @param $uids
+     * @return array
+     * @author zsy
+     */
+    public function getVerified($uids)
+    {
+        !is_array($uids) && $uids = explode(',', $uids);
+        $verifieds = $this->where(['uid' => ['in', $uids]])->field('id, uid, usergroup_id, verified')->findAll();
+
+        $data = [];
+        foreach ($verifieds as $verified) {
+            $data[$verified['uid']] = $verified;
+        }
+
+        $return = [];
+        foreach ($uids as $v) {
+            if (!$data[$v]) {
+                $return[$v] = [
+                    'id' => 0,
+                    'usergroup_id' => 0,
+                    'verified' => -1
+                ];
+            } else {
+                $return[$v] = [
+                    'id' => $data[$v]['id'],
+                    'usergroup_id' => $data[$v]['usergroup_id'],
+                    'verified' => ($data[$v]['verified'] == 1 ? 1 : ($data[$v]['verified'] == -1 ? 2: 0))
+                ];
+            }
+        }
+
+       return $return;
+    }
+
+    /**
+     * 获取用户认证详情
+     *
+     * @param $uids
+     * @return array
+     * @author zsy
+     */
+    public function getVerifyInfo($uids)
+    {
+        !is_array($uids) && $uids = explode(',', $uids);
+        if (empty($uids)) {
+
+            return [];
+        }
+        $map['uid'] = array('IN', $uids);
+        $verifieds = $this->where($map)->findAll();
+
+        $data = [];
+        foreach ($verifieds as $verified) {
+            $data[$verified['uid']] = $verified;
+        }
+
+        $return = [];
+        foreach ($uids as $v) {
+            if (!$data[$v]) {
+                $return[$v] = [
+                    'id' => 0,
+                    'usergroup_id' => 0,
+                    'verified' => -1
+                ];
+            } else {
+                // 获取认证组
+                $group_ = \Ts\Models\UserGroup::where('user_group_id', $data[$v]['usergroup_id'])->select('user_group_id', 'user_group_name', 'user_group_icon', 'is_authenticate')->first();
+                $group = [
+                    'user_group_id' => $group_->user_group_id,
+                    'user_group_name' => $group_->user_group_name,
+                    'user_group_icon' => $group_->icon,
+                    'is_authenticate' => $group_->is_authenticate,
+                ];
+
+                // 获取认证附件
+                $attachIds = array_filter(explode('|', $data[$v]['attach_id']));
+                if (!$attachIds) {
+                    $attach = [];
+                } else {
+                    $attach_ = \Ts\Models\Attach::whereIn('attach_id', $attachIds)->select('attach_id', 'save_path', 'save_name')->get();
+                    foreach ($attach_ as $kk => $vv) {
+                        $attach[$kk]['attach_id'] = $vv->attach_id;
+                        $attach[$kk]['url'] = $vv->path ?: ($vv->attach_id> 0 ? getImageUrlByAttachId($vv->attach_id) : '');
+                    }
+                }
+
+                $return[$v] = [
+                    'id' => $data[$v]['id'],
+                    'verified' => ($data[$v]['verified'] == 1 ? 1 : ($data[$v]['verified'] == -1 ? 2: 0)),
+                    'usergroup' => $group,
+                    'user_verified_category' => D('user_verified_category')->where('user_verified_category_id='.$data[$v]['user_verified_category_id'])->field('user_verified_category_id,title')->find(),
+                    'company' => $data[$v]['company'],
+                    'realname' => $data[$v]['realname'],
+                    'idcard' => $data[$v]['idcard'],
+                    'phone' => $data[$v]['phone'],
+                    'info' => $data[$v]['info'],
+                    'attach_id' => array_values($attach),
+                    'reason' => $data[$v]['reason'],
+                ];
+
+                unset($attach, $attach_, $attachIds);
+            }
+        }
+
+
+        return $return;
+    }
+}
--- UpdateModel.py
+++ UpdateModel.py
@@ -3,18 +3,119 @@
 
 import os
 import sys
+from subprocess import *
 
-ServerDir = "~/home/wwwroot/"
-ServerBakDir = "~/home/WebBackup/"
+ServerDir = "/home/wwwroot/"
+ServerUpdateFileDir = 'UpdateFile'
+ServerBakDir = "BackupFile"
 
 def get_foward_dir():
 	return os.getcwd()
 
-def read_dir():
-	pass
+def read_update_dir():
+	dirlist = []
+	for i in os.walk("."):
+		if i[-1] != [] and i[0] != ".":
+			dirlist.append(i)
+			#print(dirlist)
+	dirlist_length = len(dirlist)
+	#print(dirlist_length)
+	list_num = 0
+	while list_num < dirlist_length:
+		update_dir = dirlist[list_num][0]
+		print('-'*50)
+		print("需要更新的目录%d：\033[4;31m %s \033[0m"%((list_num+1),update_dir))
+		update_file = dirlist[list_num][-1]
+		print("需要更新的文件列表%d：%s"%((list_num+1),update_file))
+		list_num+=1
+
+def read_updated_dir():
+	dirlist = []
+	for i in os.walk("."):
+		if i[-1] != [] and i[0] != ".":
+			dirlist.append(i)
+			#print(dirlist)
+	dirlist_length = len(dirlist)
+	#print(dirlist_length)
+	list_num = 0
+	while list_num < dirlist_length:
+		update_dir = dirlist[list_num][0]
+		print('-'*50)
+		print("已更新的目录%d：\033[1;33m %s \033[0m"%((list_num+1),update_dir))
+		update_file = dirlist[list_num][-1]
+		print("已更新的文件列表%d：\033[1;31m %s \033[0m"%((list_num+1),update_file))
+		list_num+=1
 
 def read_bak_dir():
 	pass
 
-def main():
+def do_update_file():
 	pass
+
+def do_backup_file():
+	pass
+
+def restart_server():
+	pass
+
+def main():
+	
+	update_flag = True
+	forward_dir = get_foward_dir()
+	os.chdir(("."+os.sep+ServerUpdateFileDir))
+	while True:
+		os.system('clear')
+		print("****************   红企家园网站-更新程序  v1.01   *****************")
+		print("当前目录：\033[1;33;40m %s \033[0m"%forward_dir)
+		#print("目录输出的类型：",type(forward_dir))
+		#print("执行os.listdir(),结果。。。\n",os.listdir())
+
+		for x in os.listdir():
+			if os.path.isdir(x):
+				if x == ServerUpdateFileDir:
+					print("命中\033[1;31m %s \033[0m文件夹！"%ServerUpdateFileDir)
+
+		print("当前工作的文件夹：%s"%get_foward_dir())
+		
+		if update_flag:
+			read_update_dir()
+			print('*'*50)
+			update_usr_input = input("\033[1;31m 注意 \033[0m是否开始更新文件：\n 1.开始更新 \n 2.不更新 \n 3.退出更新程序 \n 请选择(数字键1/2/3，默认3)：")
+			if update_usr_input == "1":
+				print("\033[1;31m 开始更新，请勿进行其他操作... \033[0m")
+				#这里写更新代码
+				print("更新成功完成！")
+				update_flag = False
+				input("")
+				continue
+			elif update_usr_input == "2":
+				print("未更新任何文件！")
+				input("")
+				continue
+			elif update_usr_input == "3":
+				os.system('clear')
+				print("系统退出！")
+				exit()
+			else:
+				os.system('clear')
+				print("系统退出！")
+				exit()
+		else:
+			print('*'*50)
+			update_usr_input = input("\033[1;31m 注意 \033[0m网站已经更新完成：\n 1.查看更新过的目录 \n 2.退出更新程序 \n 请选择(数字键1/2，默认2)：")
+			if update_usr_input == "1":
+				read_updated_dir()
+				input("")
+				continue
+			elif update_usr_input == "2":
+				os.system('clear')
+				print("系统退出！")
+				exit()
+			else:
+				os.system('clear')
+				print("系统退出！")
+				exit()
+
+
+if __name__ == '__main__':
+	main()
